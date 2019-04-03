084b3cd72e2a742dc70d056111f07631698e3a43
amnguyen <amnguyen@sg.ibm.com>
2019-04-03 11:35:20 +0800

    first commit

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ express()
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.postgres://wppudawzdnutie:d2091f922901f53d65c28037ae5f01702e0c0dc8fb957eb39b15467fa1923124@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d7as6ofv8qna9c
-  ,
+  connectionString: process.env.DATABASE_URL || 'postgres://wppudawzdnutie:d2091f922901f53d65c28037ae5f01702e0c0dc8fb957eb39b15467fa1923124@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d7as6ofv8qna9c',
   ssl: true,
 });
 
