d0d9a1638eca13683001a8f7bf5b927967c10b67
amnguyen <amnguyen@sg.ibm.com>
2019-04-03 11:11:36 +0800

    first commit

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.postgres://wppudawzdnutie:d2091f922901f53d65c28037ae5f01702e0c0dc8fb957eb39b15467fa1923124@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d7as6ofv8qna9c
+  ,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
