f20cea15e60587ce063d32675e19d0b6aa3b79c6
amnguyen <amnguyen@sg.ibm.com>
2019-04-04 10:39:30 +0800

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-console.log("test");
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -12,20 +11,24 @@ express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
-const { Client } = require('pg');
+const { Pool, Client } = require('pg')
+const connectionString = 'postgres://wppudawzdnutie:d2091f922901f53d65c28037ae5f01702e0c0dc8fb957eb39b15467fa1923124@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d7as6ofv8qna9c'
+
+const pool = new Pool({
+  connectionString: connectionString,
+})
+
+pool.query('SELECT Id, FirstName, LastName FROM salesforce.contact;', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgres://wppudawzdnutie:d2091f922901f53d65c28037ae5f01702e0c0dc8fb957eb39b15467fa1923124@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d7as6ofv8qna9c',
-  ssl: true,
-});
-
-client.connect();
+  connectionString: connectionString,
+})
+client.connect()
 
 client.query('SELECT Id, FirstName, LastName FROM salesforce.contact;', (err, res) => {
-
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+  console.log(err, res)
+  client.end()
+})
