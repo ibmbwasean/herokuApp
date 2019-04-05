ad5f7ae8c9f94b3b66043f6043712ea8a00c4ebd
amnguyen <amnguyen@sg.ibm.com>
2019-04-05 16:47:20 +0800

    third commit

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+const { Client } = require('pg');
+const bodyParser = require('body-parser');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .use(bodyParser.urlencoded({extended: false}))
+  .use(bodyParser.json())
+
+
+
+  .post('/ProfileDisplay', (req,res) => {
+  		var email = request.body.email;
+
+  		const client = new Client({
+	  	connectionString: process.env.DATABASE_URL || 'postgres://wppudawzdnutie:d2091f922901f53d65c28037ae5f01702e0c0dc8fb957eb39b15467fa1923124@ec2-23-23-173-30.compute-1.amazonaws.com:5432/d7as6ofv8qna9c',
+	  	ssl: true,
+		});
+
+		client.connect();
+
+		client.query('SELECT * FROM Contact WHERE email = $1', [email], (error, results) => {
+			if (err) throw err;
+			res.render('pages/ProfileDisplay', {
+				results : JSON.stringify(results.rows[0])
+			});
+			client.end();
+		})
+
+
+  })
+
+
+
+
+
 
   .get('/retrieveData',(req,res) => {
-	const { Client } = require('pg');
+	
 	var a = [];
 	var b = ''
 	const client = new Client({
@@ -39,5 +70,8 @@ express()
 
 
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+
+   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
